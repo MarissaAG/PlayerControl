fcf446290f9b5a4a9e15b862023fbba453734b3f
MarissaAG <747121@student.dusd.net>
2023-02-16 09:53:58 -0800

    player control
    added code so the player can control the car.

--- PlayerControl/Assets/Scripts/PlayerContoller.cs
+++ PlayerControl/Assets/Scripts/PlayerContoller.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+   private float forwardInput;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,10 @@ public class PlayerContoller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //move the vehicle forward
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
